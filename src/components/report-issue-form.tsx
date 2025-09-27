38b55da981cc44fd51eac83aec24cf7e8af02ead
NIVESH VARUN M IT student <niveshvarunm23it@psnacet.edu.in>
2025-09-27 16:34:12 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/components/report-issue-form.tsx
+++ src/components/report-issue-form.tsx
@@ -22,7 +22,7 @@ import { PawPrintIcon } from "@/components/icons/logo";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
